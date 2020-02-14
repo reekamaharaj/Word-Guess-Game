58ed47ffd1ee3199b1a9bffc87422aa69b761c31
Reeka <reekamaharaj@gmail.com>
2020-02-14 11:40:18 -0600

    work on the js logic, set up checking the guesses to see if they are in the word or if they have been guesses already.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -7,7 +7,7 @@ let userGuess = 0;
 let guesses = [ ];
 
 // Keep track of the correct guesses
-let correctguesses = [ ];
+let correctGuesses = [ ];
 
 // Keep track of number of wins
 let win = 0;
@@ -18,73 +18,112 @@ let guessCount;
 // Keep track of loss
 let loss = 0;
 
-// Array of the possible options that are acceptable guesses
-const abc=["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z"];
+//randomly chosen word
+let word; 
 
-// Array of possible words
+// Array of the possible options that are acceptable guesses
+const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+
+
+
+//Array of possible words 
+
+const words = ['ANGORA' , 'ALLEY' , 'TIGER' , 'BOBCAT' , 'BURMESE' , 'CALICO' , 'TABBY' , 'LEOPARD' , 'CHEETAH' , 'COUGAR' , 'GINGER' , 'FELINE' , 'HIMALAYAN' , 'JAGUAR' , 'KITTEN' , 'LION' , 'LYNX' , 'MANX' , 'WEDGIE', 'OCELOT' , 'PANTHER' , 'PERSIAN' , 'PUMA' , 'SIAMESE' , 'TOM' , 'TIGER']
 
 //Set up for what will happen when a key is pressed
 document.onkeyup = function(event){
     userGuess = event.key;
-    userGuess = userGuess.toLowerCase();
+    userGuess = userGuess.toUpperCase();
 
-    if(userGuess != 0){
+    if (word === undefined){
 
-        // Check to see that the userGuess is valid. If it isn't, then an alert will ask for a letter
-        if (abc.includes(userGuess){
+        word = words[Math.floor(Math.random()*words.length)];
+        console.log(word);
 
-            if(guesses.includes(userGuess)){
-                alert('Already guessed');
+    }
+
+    if (userGuess != 0){
+
+        if (abc.includes(userGuess)){
+
+            if (word.includes(userGuess) === true ){
+
+                if (correctGuesses.includes(userGuess)){
+                    alert('Already guessed');
+                }
+
+                else {
+                console.log('thats in the word');
+                correctGuesses.push(userGuess);
+                console.log(correctGuesses);
+
+                }
+
+                //will need to put in what happens after finding the letter is in the word
             }
 
-        })
+            else {
+
+                if (guesses.includes(userGuess)){
+                    alert('Already guessed');
+                }
+
+                else {
+                    guessCount--;
+                    guesses.push(userGuess);
+                    console.log('try again');
+                }
+            }
+        }
 
         else {
             alert('Enter a letter');
         }
+
+    }
+
 
         // If the guessCount get to zero, then the game is over, the guesses are erased, guessCount is reset and a new word is generated. Game starts over
         if (guessCount === 0) {
             loss++;
             guessCount = 10; //Or whatever it is suppoed to be...
             guesses = [ ];
-
+            document.getElementById("loss").innerText = loss;
+            document.getElementById("guessesLeft").innerText = guessCount;
+            document.getElementById("win").innerText = win;
+            word = undefined;
             alert("Sorry, you didn't guess the word.");
         }
 
     }
-}
-
-// below, this will add the number of guesses left to the HTML page
-// document.getElementById("guessesLeft").innerText = guessCount;
-
-
-// below, this will add the number of losses to the HTML page
-// document.getElementById("loss").innerText = loss;
-
 
 /*  Word guess game
-key events to listen for letter the player guesses -> same as the previous one. 
+TODO:
+[x] event key to listen for letter the player guesses -> same as the previous one. 
 
-make an array for the words the computer can choose from, random choice will be picked from the array, that word will be saved in a variable and then split up into single variables. 
+[x] make an array for the words the computer can choose from
+[x] random choice will be picked from the array of words
 
-Set up a visual that shows how many letters are in the word, when word is guessed correctly, the letter will be shown.
+- Set up a visual that shows how many letters are in the word
+- When letter is guessed correctly, the letter will be shown in the location it is in the word
 
-Check to see if userGuess is in the word
-if yes, then get another guess, will display where they letter was correct, if no then the guesses count will go down by one. if userGuess is in word multiple times, take note. 
+[x] Check to see if userGuess is in the word
+[x] if yes, then get another guess, 
+- if no then the guesses count will go down by one. 
+- if userGuess is in word multiple times, account for that
+[x] User will be notifed if they have guessed the same thing already
 
-if the word is 'madonna' display it like:
+- example of visual if the word is 'madonna' display it like:
 _ _ _ _ _ _ _ when the game starts and fill it in as the game progresses
 'm a d o _ _ a'
 
 -- extra --
 
-Play a sound or song when the user guesses the word
-Design to match the game, set a game theme
-Make game code an object.
-Save game and its properties in an object
-Save game functions as methods, call them under object declaration using event listeners
-
--- Add homeworks to portfolio with linkes to the github pages
+- Play a sound or song when the user guesses the word
+- Design to match the game, set a game theme
+- Make game code an object.
+- Save game and its properties in an object
+- Save game functions as methods, call them under object declaration using event listeners
+- Add homeworks to portfolio with linkes to the github pages
 
 */
