3f7bdc35b2e6eb03e89a84ac4d3fe9b6073d1ab8
Reeka <reekamaharaj@gmail.com>
2020-02-14 23:33:48 -0600

    game working well, runs smoothly

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -27,6 +27,9 @@ let solution = [ ];
 //length of word
 let wordLength;
 
+//is the game started
+let gameStarted = false;
+
 // Array of the possible options that are acceptable guesses
 const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
 
@@ -34,101 +37,125 @@ const abc = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q"
 
 const words = ['ANGORA' , 'ALLEY' , 'TIGER' , 'BOBCAT' , 'BURMESE' , 'CALICO' , 'TABBY' , 'LEOPARD' , 'CHEETAH' , 'COUGAR' , 'GINGER' , 'FELINE' , 'HIMALAYAN' , 'JAGUAR' , 'KITTEN' , 'LION' , 'LYNX' , 'MANX' , 'WEDGIE', 'OCELOT' , 'PANTHER' , 'PERSIAN' , 'PUMA' , 'SIAMESE' , 'TOM' , 'TIGER']
 
-//Set up for what will happen when a key is pressed
+//Set up for what will happen when a key is pressed0 (General keys)
+
 document.onkeyup = function(event){
-    userGuess = event.key;
-    userGuess = userGuess.toUpperCase();
+        userGuess = event.key;
+        userGuess = userGuess.toUpperCase();
 
-    if (word === undefined){
+        if (!gameStarted) {
+            if (userGuess == " ") {
+                gameStarted = true;
+            } else {
+                return;
+            }
+        }
 
-        word = words[Math.floor(Math.random()*words.length)];
+        gameStart();
+            //creates an onscreen visual that shows how many letters are in the word
+
+        if (userGuess != 0){
+
+            //checks to see if the guess is valid
+            if (abc.includes(userGuess)){
+                //if the guess is valid, checks to see if the guess is in the word
+                if (word.includes(userGuess) === true ){
+                    //if the guess has already been guessed it will say so
+                    if (correctGuesses.includes(userGuess)){
+                        alert('Already guessed');
+                    }
+                    //the guess will be added into an array so that the correct guess cant be guessed again
+                    else {
+                    correctGuesses.push(userGuess);
+                        // Will check where the letter is in the array and change the _ to the letter
+                        letterCorrect();
+                        checkIfWin();
+
+                    }
+                }
+
+                else {
+                    //checks if the letter has already been guessed
+                    if (guesses.includes(userGuess)){
+                        alert('Already guessed');
+                    }
+                    //if the guess is new and wrong then it will reduce the guessCout
+                    else {
+                        guessCount--;
+                        guesses.push(userGuess);
+                        document.getElementById("guessesLeft").innerText = guessCount;
+                        document.getElementById("guesses").innerText = guesses;
+                    }
+                }
+            }
+
+            else {
+                alert('Enter a letter');
+            }
+
+        }
+
+            // If the guessCount get to zero, then the game is over, the guesses are erased, guessCount is reset and a new word is generated. Game starts over
+            if (guessCount === 0) {
+                loss++;
+                alert("Sorry, you didn't guess the word.");
+                reset();
+                gameStart()
+            }
+
+}
+
+    
+//functions
+
+function letterCorrect() {
+    for (var x = 0; x < word.length; x++) {
+        if (word[x] === userGuess) {
+            solution[x] = userGuess;
+            document.getElementById("word").innerText = solution.join('  ');
+            wordLength--;
+        }
+    }
+}
+
+function checkIfWin() {
+    if (wordLength === 0) {
+        win++;
+        document.getElementById("word").innerText = solution.join('  ');
+        reset();
+        setTimeout(function () {
+            alert('You win!');
+        }, 0);
+    }
+}
+
+function gameStart() {
+    if (word === undefined) {
+        word = words[Math.floor(Math.random() * words.length)];
         console.log(word);
-
-        for (var i=0; i < word.length; i++){
+        for (var i = 0; i < word.length; i++) {
             solution[i] = "_ ";
         }
         wordLength = word.length;
         document.getElementById("word").innerText = solution.join('  ');
     }
-        //creates an onscreen visual that shows how many letters are in the word
-
-    if (userGuess != 0){
-
-        //checks to see if the guess is valid
-        if (abc.includes(userGuess)){
-            //if the guess is valid, checks to see if the guess is in the word
-            if (word.includes(userGuess) === true ){
-                //if the guess has already been guessed it will say so
-                if (correctGuesses.includes(userGuess)){
-                    alert('Already guessed');
-                }
-                //the guess will be added into an array so that the correct guess cant be guessed again
-                else {
-                correctGuesses.push(userGuess);
-                    // Will check where the letter is in the array and change the _ to the letter
-
-                    for (var i =0; i < word.length; i++){
-                        if (word[i] === userGuess){
-                            solution[i] = userGuess;
-                            document.getElementById("word").innerText = solution.join('  ');
-                            wordLength--;
-                        }
-                    }
-
-                    if (wordLength === 0){
-                        win++;
-                        document.getElementById("word").innerText = solution.join('  ');
-                        alert('You win!');
-                        reset();
-                    }
-
-                }
-            }
-
-            else {
-                //checks if the letter has already been guessed
-                if (guesses.includes(userGuess)){
-                    alert('Already guessed');
-                }
-                //if the guess is new and wrong then it will reduce the guessCout
-                else {
-                    guessCount--;
-                    guesses.push(userGuess);
-                    document.getElementById("guessesLeft").innerText = guessCount;
-                    document.getElementById("guesses").innerText = guesses;
-                }
-            }
-        }
-
-        else {
-            alert('Enter a letter');
-        }
-
-    }
-
-
-        // If the guessCount get to zero, then the game is over, the guesses are erased, guessCount is reset and a new word is generated. Game starts over
-        if (guessCount === 0) {
-            loss++;
-            alert("Sorry, you didn't guess the word.");
-            reset();
-        }
-
-    }
+    return i;
+}
 
     function reset() {
+        userGuess = undefined;
         guessCount =  10;
         guesses = [ ];
         correctGuesses = [ ];
         word = undefined;
-        solution = [ ];
-        wordLength = 0;
+        wordLength = undefined;
         document.getElementById("loss").innerText = loss;
         document.getElementById("guessesLeft").innerText = guessCount;
         document.getElementById("win").innerText = win;
         document.getElementById("guesses").innerText = guesses;
-
+        gameStarted = false;
     }
+
 /*  Word guess game
 TODO:
 [x] event key to listen for letter the player guesses -> same as the previous one. 
@@ -136,7 +163,7 @@ TODO:
 [x] make an array for the words the computer can choose from
 [x] random choice will be picked from the array of words
 [x] Set up a visual that shows how many letters are in the word
-- When letter is guessed correctly, the letter will be shown in the location it is in the word
+[x] When letter is guessed correctly, the letter will be shown in the location it is in the word
 [x] Check to see if userGuess is in the word
 [x] if yes, then get another guess, 
 [x] if no then the guesses count will go down by one. 
@@ -151,9 +178,6 @@ _ _ _ _ _ _ _ when the game starts and fill it in as the game progresses
 
 - Play a sound or song when the user guesses the word
 - Design to match the game, set a game theme
-- Make game code an object.
-- Save game and its properties in an object
-- Save game functions as methods, call them under object declaration using event listeners
 - Add homeworks to portfolio with linkes to the github pages
 
 */
